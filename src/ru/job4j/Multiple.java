b81e9cf5ceecccb93bb6d1ccd7ff3a026b0a8886
ITAL <it-al@yandex.ru>
2020-08-30 16:20:07 +0300

    3.3. Разбор первой программы. [#350035]

---
+++ src/ru/job4j/Multiple.java
@@ -0,0 +1,15 @@
+package ru.job4j;
+
+public class Multiple {
+
+    public static void main(String[] args) {
+
+        int Multiplier = 9;
+        int count = 9;
+        int i;
+
+        for (i = 1 ; i < count; i++) {
+            System.out.println(i + " * " + Multiplier + " = " + i * Multiplier);
+        }
+    }
+}
