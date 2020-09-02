a78f1553c26c51bbdec6ddf46e15324353893f9f
ITAL <it-al@yandex.ru>
2020-09-02 14:40:45 +0300

    Глупый бот [#349961]

---
+++ test/ru/job4j/condition/DummyBotTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class DummyBotTest {
+
+    @Test
+    public void whenBotTest() {
+        assertThat(
+                DummyBot.answer("Привет, Бот."),
+                is("Привет, умник."));
+    }
+
+    @Test
+    public void whenByeBot() {
+        assertThat(
+                DummyBot.answer("Пока."),
+                is("До скорой встречи."));
+    }
+}
