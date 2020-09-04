fb164763d7a82ef7836a0fdc36cc9f6efb3d0972
ITAL <it-al@yandex.ru>
2020-09-04 15:03:52 +0300

    Простое число [#350073]

---
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,15 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+
+    public static boolean check(int number) {
+        boolean primeNumber = number != 1;
+        for (int i = 2; i < number; i++) {
+            if ((number % i) == 0) {
+                primeNumber = false;
+                break;
+            }
+        }
+        return primeNumber;
+    }
+}
