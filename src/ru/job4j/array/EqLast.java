88ff3703462d89216106429f8d4841ab617a694a
ITAL <it-al@yandex.ru>
2020-09-08 15:50:07 +0300

    Сравнить последние элементы двух массивов [#350004]

---
+++ src/ru/job4j/array/EqLast.java
@@ -0,0 +1,7 @@
+package ru.job4j.array;
+
+public class EqLast {
+    public static boolean check(int[] left, int[] rigth) {
+        return left[left.length - 1] == rigth[rigth.length - 1];
+    }
+}
