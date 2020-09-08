45c032da6081d05e3071d94ae51edd3e7cb85c90
ITAL <it-al@yandex.ru>
2020-09-08 17:15:56 +0300

    Двухмерный массив. Вывод на консоль. [#350003]

---
+++ src/ru/job4j/array/Sequence.java
@@ -0,0 +1,13 @@
+package ru.job4j.array;
+
+public class Sequence {
+
+    public static void main(String[] args) {
+        int[][] array = new int[9][9];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[0].length; j++) {
+                array[i][j] = j + 1;
+            }
+        }
+    }
+}
