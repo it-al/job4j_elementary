87c2f218d9f3ff0d38fe0940c195d943d7288e62
ITAL <it-al@yandex.ru>
2020-09-11 09:13:48 +0300

    Размер элемента двухмерного массива. [#350009] исправления

--- src/ru/job4j/array/LengthArrayArrays.java
+++ src/ru/job4j/array/LengthArrayArrays.java
@@ -2,16 +2,14 @@ package ru.job4j.array;
 
 public class LengthArrayArrays {
     public static void main(String[] args) {
-        int[][][][] numbers =
-                {{{{1, 1, 1, 1}, {1, 1, 1, 1}, {1, 1, 1, 1}}, {{1, 1, 1, 1}, {1, 1, 1, 1}, {1, 1, 1, 1}}}};
-        for (int[][][] array3: numbers) {
-            System.out.println("Размер вложенного массива равен: " + array3.length);
-            for (int[][] array2 : array3) {
-                System.out.println("Размер вложенного массива равен: " + array2.length);
-                for (int[] array1 : array2) {
-                    System.out.println("Размер вложенного массива равен: " + array1.length);
-                }
-            }
+        int[][] numbers = {
+                {1},
+                {3, 4},
+                {5, 6, 7},
+                {8, 9, 10, 11}
+        };
+        for (int[] array : numbers) {
+            System.out.println("Размер вложенного массива равен: " + array.length);
         }
     }
 }
