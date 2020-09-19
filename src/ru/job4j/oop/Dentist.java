226fbfd2e0fcd8f9b1007d027cd7823c6ff42f13
ITAL <it-al@yandex.ru>
2020-09-19 12:19:57 +0300

    Реализация профессий в коде [#350193]

---
+++ src/ru/job4j/oop/Dentist.java
@@ -0,0 +1,9 @@
+package ru.job4j.oop;
+
+public class Dentist extends Doctor {
+    private int cost;
+
+    public int getCost() {
+        return 0;
+    }
+}
---
+++ src/ru/job4j/oop/Doctor.java
@@ -0,0 +1,14 @@
+package ru.job4j.oop;
+
+public class Doctor extends Profession {
+    private boolean academicDegree;
+
+    public Diagnosis heal(Pacient pacient) {
+        Diagnosis result = new Diagnosis();
+        return result;
+    }
+
+    public void sendDiagnos(Pacient pacient) {
+
+    }
+}
