27208f71c426ebede81689073b33e098fcc78b76
ITAL <it-al@yandex.ru>
2020-09-21 12:38:57 +0300

    Почему наследование - это плохо [#350190]

---
+++ src/ru/job4j/oop/Product.java
@@ -0,0 +1,24 @@
+package ru.job4j.oop;
+
+public class Product {
+    private String name;
+    private int price;
+
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public int price() {
+        return price - discont();
+    }
+
+    public int discont() {
+        return 0;
+    }
+
+    public String label() {
+        return name + " " + price();
+    }
+}
+
