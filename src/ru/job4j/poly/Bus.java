ad1a09b939ef6f9417223f158003215249f3a04a
ITAL <it-al@yandex.ru>
2020-09-23 16:00:58 +0300

    Интерфейс Input [#350124]

---
+++ src/ru/job4j/poly/Bus.java
@@ -0,0 +1,19 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+
+    @Override
+    public void go() {
+
+    }
+
+    @Override
+    public void setPassengers(int passengers) {
+
+    }
+
+    @Override
+    public int refuel(int fuel) {
+        return 0;
+    }
+}
