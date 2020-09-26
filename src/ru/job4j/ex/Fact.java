7f59edc883fe8601a070ab586b79c46033924461
ITAL <it-al@yandex.ru>
2020-09-26 23:27:28 +0300

    Кидаем исключение - throw new RuntimeException [#350142]

---
+++ src/ru/job4j/ex/Fact.java
@@ -0,0 +1,18 @@
+package ru.job4j.ex;
+
+public class Fact {
+    public static void main(String[] args) {
+
+    }
+
+    public int calc(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Value cannot be negative");
+        }
+        int rsl = 1;
+        for (int index = 1; index <= n; index++) {
+            rsl *= index;
+        }
+        return rsl;
+    }
+}
