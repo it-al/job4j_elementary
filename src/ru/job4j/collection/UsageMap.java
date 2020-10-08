b3b3477750a11a723ff723832d881050db1ceca8
ITAL <it-al@yandex.ru>
2020-10-08 11:48:39 +0300

    Map, HashMap [#350109]

---
+++ src/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,15 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("it-al@yandex.ru", "Никитин А.С");
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+        }
+    }
+}
