00cd1a473f9bf8ca2b3f85990a2423048f477902
ITAL <it-al@yandex.ru>
2020-10-09 12:26:43 +0300

    Паспорт и жители [#350110] исправление

--- src/ru/job4j/collection/PassportOffice.java
+++ src/ru/job4j/collection/PassportOffice.java
@@ -4,18 +4,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PassportOffice {
-    private Map<String, String> citizens = new HashMap<>();
+    private Map<String, Citizen> citizens = new HashMap<>();
 
     public boolean add(Citizen citizen) {
         boolean rsl = false;
         if (!citizens.containsKey(citizen.getPassport())) {
-            citizens.put(citizen.getPassport(), citizen.getUsername());
+            citizens.put(citizen.getPassport(), citizen);
             rsl = true;
         }
         return rsl;
     }
 
     public Citizen get(String passport) {
-        return new Citizen(passport, citizens.get(passport));
+        return citizens.get(passport);
+
     }
 }
