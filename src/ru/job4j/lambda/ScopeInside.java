58729754a8006759c89cf32060483965b7ff9c6a
ITAL <it-al@yandex.ru>
2020-10-22 16:19:17 +0300

    Зона видимости в лямбда-выражениях и исключения [#349928]

---
+++ src/ru/job4j/lambda/ScopeInside.java
@@ -0,0 +1,22 @@
+package ru.job4j.lambda;
+
+import java.util.function.Supplier;
+
+public class ScopeInside {
+    static int total = 0;
+
+    public static void main(String[] args) {
+        int[] number = {1, 2, 3};
+        for (int i = 0; i < number.length; i++) {
+            int num = i;
+            total = add(
+                    () -> total + num
+            );
+        }
+        System.out.println(total);
+    }
+
+    private static Integer add(Supplier<Integer> calc) {
+        return calc.get();
+    }
+}
