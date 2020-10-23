c99080f6ef010a69c7a17b534a4c274133ffd959
ITAL <it-al@yandex.ru>
2020-10-23 15:10:23 +0300

    Список адресов [#349940]

---
+++ src/ru/job4j/stream/Profile.java
@@ -0,0 +1,18 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Profile {
+    private Address address;
+
+    public Profile(Address address) {
+        this.address = address;
+    }
+
+    public static List<Address> collect(List<Profile> profiles) {
+        return profiles.stream()
+                .map((s) -> s.address)
+                .collect(Collectors.toList());
+    }
+}
---
+++ test/ru/job4j/stream/ProfileTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.stream;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class ProfileTest {
+    private List<Profile> profiles = new ArrayList<>();
+
+    @Before
+    public void setUp() {
+        profiles.add(new Profile(new Address("Krasnodar", "Krasnay", 15, 40)));
+    }
+
+    @Test
+    public void whenAddressTest() {
+        List<Address> rsl = Profile.collect(profiles);
+        List<Address> expected = new ArrayList<>();
+        expected.add(new Address("Krasnodar", "Krasnay", 15, 40));
+        assertThat(rsl, is(expected));
+    }
+}
