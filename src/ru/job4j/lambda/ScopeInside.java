67f121c11e51b0ae641b487f1505faacd1c5152a
ITAL <it-al@yandex.ru>
2020-10-23 11:30:38 +0300

    Зона видимости в лямбда-выражениях и исключения [#349928]

--- src/ru/job4j/lambda/ScopeInside.java
+++ src/ru/job4j/lambda/ScopeInside.java
@@ -1,22 +1,17 @@
 package ru.job4j.lambda;
 
-import java.util.function.Supplier;
+import java.util.function.BinaryOperator;
 
 public class ScopeInside {
-    static int total = 0;
 
     public static void main(String[] args) {
         int[] number = {1, 2, 3};
+        int total = 0;
+        BinaryOperator<Integer> calc = (integer, integer2) -> integer + integer2;
         for (int i = 0; i < number.length; i++) {
-            int num = i;
-            total = add(
-                    () -> total + num
-            );
+            int num = number[i];
+            total = calc.apply(total, num);
         }
         System.out.println(total);
-    }
-
-    private static Integer add(Supplier<Integer> calc) {
-        return calc.get();
     }
 }
